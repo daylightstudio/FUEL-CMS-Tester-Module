2b7e0b3cf50c7c00a2876600f75a0e2af5a77fa5
Martynas Sateika <martynas.sateika@gmail.com>
2018-10-03 00:40:29 +0100

    Fix broken link to CodeIgniter forum thread

--- views/_docs/index.php
+++ views/_docs/index.php
@@ -14,7 +14,7 @@ php index.php tester/run fuel Fuel_cache_test.php
 <p class="important">If you are on a Mac and having trouble where the script is outputting nothing, you may need to make sure 
 you are calling the right php binary. For example, you may need to use something like /Applications/MAMP/bin/php/php5.3.6/bin/php.
 Here is a thread that talks about it more:
-<a href="http://codeigniter.com/forums/viewthread/130383/" target="_blank">http://codeigniter.com/forums/viewthread/130383/</a>
+<a href="https://forum.codeigniter.com/thread-23025.html" target="_blank">https://forum.codeigniter.com/thread-23025.html</a>
 Hopefully it saves you some time too!
 </p>
 
